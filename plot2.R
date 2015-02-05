6906380d5fb397c9f907342d707915df87950503
Susan Z Martin <SusanZMartin@users.noreply.github.com>
2015-02-05 12:58:19 -0800

    Update plot2.R
    Added comment that tells reader where to start looking for the code that creates the plot and also made comments more explanatory.

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+## note that code used to create plot starts on line 50 if you want to skip to there
+
 ## The code below reads in the data from the original zip file (obtained from
 ## https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 ## on February 3, 2015) once it is unzipped and stored in your working directory
@@ -7,6 +9,8 @@
 
 
 ## read in a subset of the big dataset with a variety of settings
+## included row 2881 (third day of February) because I thought it might be needed
+## to make plots display Saturday
 
 householdpwr <- read.csv2("household_power_consumption.txt", sep=";", quote = "\"", skip = 66636, nrows = 2881, stringsAsFactors = FALSE )
 
@@ -15,9 +19,8 @@ householdpwr <- read.csv2("household_power_consumption.txt", sep=";", quote = "\
 
 names(householdpwr) <- c("OldDate", "OldTime", "GlobalActivePower","GlobalReactivePower", "Voltage", "GlobalIntensity", "SubMetering1", "SubMetering2", "SubMetering3")
 
-## add a zero to month numbers so that 30/1/2007 becomes 30/01/2007 - create a new column
-## and cbind it to the existing data frame - also need to add a 0 to day - this doesn't seem
-## to be working - check it out tomorrow (February 3, 2015)
+## add a zero to month numbers so that 30/1/2007 becomes 30/01/2007 and then
+## convert to proper format using as.Date
 
 testHHpwr <- gsub("/2/","/02/", householdpwr[,1])
 testHHpwr <- gsub("2/02","02/02", testHHpwr)
@@ -25,31 +28,29 @@ testHHpwr <- gsub("3/02","03/02", testHHpwr)
 testHHpwr <- gsub("4/02","04/02", testHHpwr)
 newdates <- as.Date(testHHpwr, "%d/%m/%Y")
 
-## fix times as per intstructions - not quite sure yet why I need to fix times
+## create another vector with the times
 
 newtimes <- householdpwr[,2]
+
+## paste times and dates together to create a properly formatted variable and then cbind
+## with rest of dataset
 
 x <- paste(newdates, newtimes)
 
 newdateandtime <- as.POSIXct(x)
 
-
-
-
-
-## cbind the combined date and time to householdpwr and then add a weekday variable
-
-
 newHHpwr <- cbind(newdateandtime, householdpwr)
+
+## Added in a weekday field because I thought it might be needed for the plots
+## it wasn't but I just left it
 
 newHHpwr <- mutate(newHHpwr, weekday = wday(newdateandtime, label=TRUE))
 
-## create plot2 
+
+## creates plot2.png
+
+png(file = "plot2.png", width = 480, height = 480)
 
 plot(newHHpwr$newdateandtime, as.numeric(newHHpwr$GlobalActivePower), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-
-## copy to file as 480 x 480 pixels
-
-dev.copy(png, file = "plot2.png", width = 480, height = 480)
 
 dev.off()
